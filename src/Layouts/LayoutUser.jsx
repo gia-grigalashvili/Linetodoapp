b80fd7884dd354d98cf63ba4487fcedf17f6bb26
gia-grigalashvili <grigalagia07@gmail.com>
2024-12-04 22:40:06 +0400

    clerk

--- src/Layouts/LayoutUser.jsx
+++ src/Layouts/LayoutUser.jsx
@@ -1,14 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Pattern from "../components/Patterns";
 
 export default function LayoutUser() {
   return (
-    <div>
-      <div className={` w-[100vw] flex items-center justify-center`}>
-        <Pattern />
-        <Outlet />
-      </div>
+    <div className="w-full h-screen flex justify-between items-center gap-[175px] xl:pl-[66px] xl:pr-[45px] overflow-hidden max-w-[1280px] mx-auto">
+      <Pattern />
+      <Outlet />
     </div>
   );
 }
--- src/pages/SingUpPage.jsx
+++ src/pages/SingUpPage.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { SignUp } from "@clerk/clerk-react";
 export default function SingUpPage() {
